f2c9301a98a6af382b1258b10285c063c917376e
Philip-Owen <31570473+Philip-Owen@users.noreply.github.com>
2018-05-03 07:43:13 -0500

    Set up basic component jsx structure.

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+  state = {
+    zipCode: '',
+    apiData: [],
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Weather</h1>
+        <input />
+        <button>Get Weather</button>
+        <div>
+          <p>City</p>
+          <img src="" alt="" />
+          <p>Temp</p>
+          <p>High</p>
+          <p>Low</p>
+          <p>Wind Speed</p>
+          <p>Wind Direction</p>
+        </div>
       </div>
     );
   }
